866fd4380d498c03b634938f29648f39399aabdb
ileler <ileler@qq.com>
2018-07-07 11:57:23 +0800

    commit spring-boot-demo

---
+++ spring-boot-demo/org.ileler.services/org.ileler.zipkin/src/main/java/org/ileler/zipkin/Application.java
@@ -0,0 +1,22 @@
+package org.ileler.zipkin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Date:        2018年07月07日 上午9:40 AM
+ * Author:      kerwin612
+ * Version:     1.0.0.0
+ * Description: Initialize
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableZipkinServer
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
