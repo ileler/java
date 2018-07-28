681917fb986d90a34523c6456d32517744414089
ileler <ileler@qq.com>
2018-07-28 22:44:38 +0800

    ...

--- spring-boot-demo/org.ileler.utils/src/main/java/org/ileler/utils/IPUtils.java
+++ spring-boot-demo/org.ileler.utils/src/main/java/org/ileler/utils/IPUtils.java
@@ -1,12 +1,15 @@
 package org.ileler.utils;
 
-import java.util.Collections;
-import java.util.Enumeration;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.http.HttpHeaders;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+import org.springframework.web.reactive.function.server.ServerRequest;
 
 import javax.servlet.http.HttpServletRequest;
-
-import org.springframework.http.HttpHeaders;
-import org.springframework.web.reactive.function.server.ServerRequest;
+import java.util.Collections;
+import java.util.Enumeration;
+import java.util.List;
 
 /**
  * Author: kerwin612
@@ -20,7 +23,8 @@ public class IPUtils {
             String name = headerNames.nextElement();
             httpHeaders.addAll(name, Collections.list(request.getHeaders(name)));
         }
-        return getClientIP(httpHeaders);
+        String clientIP = getClientIP(httpHeaders);
+        return StringUtils.isBlank(clientIP) ? request.getRemoteAddr() : clientIP;
     }
 
     public static String getClientIP(ServerRequest request) {
@@ -28,11 +32,36 @@ public class IPUtils {
     }
 
     public static String getClientIP(HttpHeaders headers) {
-        return null;
+        if (headers == null)    return null;
+        String ip = clientIP(headers, "X-Forwarded-For");
+        if (ip == null || "unknown".equalsIgnoreCase(ip)) {
+            ip = clientIP(headers, "Proxy-Client-IP");
+        }
+        if (ip == null || "unknown".equalsIgnoreCase(ip)) {
+            ip = clientIP(headers, "WL-Proxy-Client-IP");
+        }
+        if (ip == null || "unknown".equalsIgnoreCase(ip)) {
+            ip = clientIP(headers, "HTTP_CLIENT_IP");
+        }
+        if (ip == null || "unknown".equalsIgnoreCase(ip)) {
+            ip = clientIP(headers, "HTTP_X_FORWARDED_FOR");
+        }
+
+        return (StringUtils.isNotBlank(ip) && ip.contains(",")) ? ip.split(",")[0] : ip;
+    }
+
+    private static String clientIP(HttpHeaders headers, String key) {
+        List<String> strings = headers == null ? null : headers.get(key);
+        return (strings == null || strings.size() < 1) ? null : strings.get(0);
     }
 
     public static String getCurrentClientIP() {
-        return null;
+        return getClientIP(getCurrentRequest());
+    }
+
+    public static HttpServletRequest getCurrentRequest() {
+        ServletRequestAttributes attrs = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+        return attrs == null ? null : attrs.getRequest();
     }
 
 }
